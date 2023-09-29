9bd0de18c2331d2e8a86e2842711eb1a3b79a35d
jmjubaer <jmjubaer3927@gmail.com>
2023-09-30 00:03:05 +0600

    basic setup Project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { AuthProvider } from '@/Providers/AuthProvider'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <AuthProvider>
+        <body className={inter.className}>
+          {children}
+        </body>
+      </AuthProvider>
     </html>
   )
 }
