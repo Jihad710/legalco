3617aceecc95061c4fbd1927da0bcb81b7537f54
Protap Ghosh <protapg518@gmail.com>
2023-10-02 23:20:33 +0600

    review code

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -13,5 +12,4 @@ const firebaseConfig = {
   appId: process.env.NEXT_APPID
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
