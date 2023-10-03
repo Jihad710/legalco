3a373b9f6fab343c608647cbb67ac012741e68c5
Protap Ghosh <protapg518@gmail.com>
2023-10-04 00:54:42 +0600

    create navbar

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,10 @@ import Navbar from '@/shareComponents/Navbar';
 
 const HomePage = () => {
   return (
-    <div className='bg-sky-100 h-[150vh]'>
-      <Navbar />
-      This is the home page
-    </div>
+     <div className="bg-sky-50 h-[150vh]">
+        <Navbar />
+        <p className='mt-10'>This is the home page</p>
+     </div>
   );
 };
 
--- src/shareComponents/Navbar.js
+++ src/shareComponents/Navbar.js
@@ -5,9 +5,26 @@ import { useState } from "react";
 import { FaBarsStaggered } from "react-icons/fa6";
 
 const Navbar = () => {
+   // Change navbar color when scrolling
+   const [color, setColor] = useState(false);
+   const changeColor = () => {
+      if (window.scrollY >= 90) {
+         setColor(true);
+      } else {
+         setColor(false);
+      }
+   };
 
-  return (
-      <div className="sticky top-0 z-50">
+   window.addEventListener("scroll", changeColor);
+
+   return (
+      <div
+         className={
+            color
+               ? "sticky top-0 z-50 duration-700 nav-bg"
+               : "sticky top-0 z-50 duration-700"
+         }
+      >
          <Container>
             <div className="drawer">
                <input
@@ -17,7 +34,7 @@ const Navbar = () => {
                />
                <div className="drawer-content flex flex-col">
                   {/* Navbar */}
-                  <div className="navbar md:py-3 w-full py-1">
+                  <div className="navbar md:py-4 w-full py-1">
                      <div className="flex-1 text-2xl md:text-3xl font-bold text-teal-600">
                         <Link href="/">LegalCo</Link>
                      </div>
@@ -73,6 +90,7 @@ const Navbar = () => {
                   </div>
                </div>
 
+               {/* Drawer side menu */}
                <div className="drawer-side">
                   <label
                      htmlFor="my-drawer-3"
@@ -107,92 +125,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-// import Link from "next/link";
-// import React from "react";
-
-// const Navbar = () => {
-//    const navOptions = (
-//       <>
-//          <li className="uppercase">
-//             <Link href="/">Home</Link>
-//          </li>
-//          <li className="uppercase">
-//             <Link href="/menu">Services</Link>
-//          </li>
-//          <li className="uppercase">
-//             <Link href="/order/salad">Appointment</Link>
-//          </li>
-//          <li className="uppercase">
-//             <Link href="/secret">Blog</Link>
-//          </li>
-//       </>
-//    );
-
-//    return (
-//       <div>
-//          <div className="flex bg-base-100">
-//             <div className="flex flex-row-reverse">
-//                <div className="drawer">
-//                   <input
-//                      id="my-drawer"
-//                      type="checkbox"
-//                      className="drawer-toggle"
-//                   />
-//                   <div className="drawer-content lg:hidden">
-//                      {/* Page content here */}
-//                      <label
-//                         htmlFor="my-drawer"
-//                         className="btn btn-primary drawer-button"
-//                      >
-//                         Open drawer
-//                      </label>
-//                   </div>
-//                   <div className="drawer-side">
-//                      <label
-//                         htmlFor="my-drawer"
-//                         aria-label="close sidebar"
-//                         className="drawer-overlay"
-//                      ></label>
-//                      <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-//                         {/* Sidebar content here */}
-//                         <li>
-//                            <a>Sidebar Item 1</a>
-//                         </li>
-//                         <li>
-//                            <a>Sidebar Item 2</a>
-//                         </li>
-//                      </ul>
-//                   </div>
-//                </div>
-//                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-//             </div>
-//             <div className="navbar-center hidden lg:flex">
-//                <ul className="menu menu-horizontal px-1">
-//                   <li>
-//                      <a>Item 1</a>
-//                   </li>
-//                   <li tabIndex={0}>
-//                      <details>
-//                         <summary>Parent</summary>
-//                         <ul className="p-2">
-//                            <li>
-//                               <a>Submenu 1</a>
-//                            </li>
-//                            <li>
-//                               <a>Submenu 2</a>
-//                            </li>
-//                         </ul>
-//                      </details>
-//                   </li>
-//                   <li>
-//                      <a>Item 3</a>
-//                   </li>
-//                </ul>
-//             </div>
-//          </div>
-//       </div>
-//    );
-// };
-
-// export default Navbar;
