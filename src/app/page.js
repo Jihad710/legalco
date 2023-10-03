50b808225ba4197d801d6217f1d84c17fdc8c599
Protap Ghosh <protapg518@gmail.com>
2023-10-04 00:57:02 +0600

    remove home page color

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Navbar from '@/shareComponents/Navbar';
 
 const HomePage = () => {
   return (
-     <div className="bg-sky-50 h-[150vh]">
+     <div className="h-[150vh]">
         <Navbar />
         <p className='mt-10'>This is the home page</p>
      </div>
