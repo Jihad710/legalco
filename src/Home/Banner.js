d8c97ac80f9af658d9200f9589bf178a4262f65e
jmjubaer <jmjubaer3927@gmail.com>
2023-10-05 09:11:06 +0600

    add review section

--- src/Home/Banner.js
+++ src/Home/Banner.js
@@ -1,9 +1,12 @@
+import SectionTitle from '@/Common/SectionTitle';
 import React from 'react';
 
 const Banner = () => {
   return (
     <div>
-            
+        <div className="text_shadow-color">
+            <SectionTitle title={"Our Services"}></SectionTitle>
+        </div>
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import HappyClient from '@/Home/HappyClient';
 import LawyerSection from '@/Home/LawyerSection';
 import ServicesPage from '@/Home/Services';
 import React from 'react';
@@ -7,7 +8,7 @@ const HomePage = () => {
     <div>
       <ServicesPage></ServicesPage>
       <LawyerSection></LawyerSection>
-      
+      <HappyClient></HappyClient>
     </div>
   );
 };
