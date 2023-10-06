d76b365833d2926142a77b2e6655a07e5cae048d
Faruk Rahman Jihad <jihadmahmud71@gmail.com>
2023-10-06 15:25:59 -0600

    contact form done

--- src/app/appoinment/page.js
+++ src/app/appoinment/page.js
@@ -1,6 +1,8 @@
 "use client"
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import contact from '../../assets/contactus.jpg'
+import Image from 'next/image';
 
 const ContactForm = () => {
   const {
@@ -19,15 +21,30 @@ const ContactForm = () => {
   const serviceInterest = watch('serviceInterest');
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="max-w-md mx-auto mt-6">
+     
+    <div className="banner-image">
+      <Image src={contact} alt="Banner" />
+
+      <div className="contact-section">
+      <div className="text-center mt-20">
+        <h2 className='text-2xl mb-2'>We Love to Hear from You</h2>
+        <p>
+        Please call or fillup appoinment form and we will be happy to assist you.
+        </p>
+      </div>
+
+      <div className="flex">
+  {/*  Form */}
+  <div className="w-2/3  mx-auto px-4">
+  <form onSubmit={handleSubmit(onSubmit)} className="max-w-md mx-auto mt-6">
       <div className="mb-4">
-        <label htmlFor="firstName" className="block text-gray-600 font-semibold">First Name</label>
+        <label  htmlFor="firstName" className="block mb-2 text-gray-600 font-semibold">First Name</label>
         <input
           {...register('firstName', { required: 'First Name is required' })}
           type="text"
           id="firstName"
           placeholder="First Name"
-          className={`w-full px-4 py-2 border rounded-lg focus:outline-none ${
+          className={`w-full px-4 py-2 border focus:outline-none ${
             errors.firstName ? 'border-red-500' : 'focus:border-blue-400'
           }`}
         />
@@ -36,13 +53,13 @@ const ContactForm = () => {
         )}
       </div>
       <div className="mb-4">
-        <label htmlFor="lastName" className="block text-gray-600 font-semibold">Last Name</label>
+        <label htmlFor="lastName" className="block mb-2 text-gray-600 font-semibold">Last Name</label>
         <input
           {...register('lastName', { required: 'Last Name is required' })}
           type="text"
           id="lastName"
           placeholder="Last Name"
-          className={`w-full px-4 py-2 border rounded-lg focus:outline-none ${
+          className={`w-full px-4 py-2 border  focus:outline-none ${
             errors.lastName ? 'border-red-500' : 'focus:border-blue-400'
           }`}
         />
@@ -51,7 +68,7 @@ const ContactForm = () => {
         )}
       </div>
       <div className="mb-4">
-        <label htmlFor="email" className="block text-gray-600 font-semibold">Email</label>
+        <label htmlFor="email" className="block mb-2 text-gray-600 font-semibold">Email</label>
         <input
           {...register('email', {
             required: 'Email is required',
@@ -63,7 +80,7 @@ const ContactForm = () => {
           type="email"
           id="email"
           placeholder="Enter your Email"
-          className={`w-full px-4 py-2 border rounded-lg focus:outline-none ${
+          className={`w-full px-4 py-2 border  focus:outline-none ${
             errors.email ? 'border-red-500' : 'focus:border-blue-400'
           }`}
         />
@@ -72,7 +89,7 @@ const ContactForm = () => {
         )}
       </div>
       <div className="mb-4">
-        <label htmlFor="phoneNumber" className="block text-gray-600 font-semibold">Phone Number</label>
+        <label htmlFor="phoneNumber" className="block mb-2 text-gray-600 font-semibold">Phone Number</label>
         <input
           {...register('phoneNumber', {
             required: 'Phone Number is required',
@@ -84,7 +101,7 @@ const ContactForm = () => {
           type="tel"
           id="phoneNumber"
           placeholder="Enter your Phone Number (11 digits)"
-          className={`w-full px-4 py-2 border rounded-lg focus:outline-none ${
+          className={`w-full px-4 py-2 border  focus:outline-none ${
             errors.phoneNumber ? 'border-red-500' : 'focus:border-blue-400'
           }`}
         />
@@ -93,11 +110,11 @@ const ContactForm = () => {
         )}
       </div>
       <div className="mb-4">
-        <label htmlFor="serviceInterest" className="block text-gray-600 font-semibold">What service are you interested in?</label>
+        <label htmlFor="serviceInterest" className="block mb-2 text-gray-600 font-semibold">What service are you interested in?</label>
         <select
           {...register('serviceInterest', { required: 'Service Interest is required' })}
           id="serviceInterest"
-          className={`w-full px-4 py-2 border rounded-lg focus:outline-none ${
+          className={`w-full px-4 py-2 border  focus:outline-none ${
             errors.serviceInterest ? 'border-red-500' : 'focus:border-blue-400'
           }`}
         >
@@ -119,12 +136,12 @@ const ContactForm = () => {
       </div>
       {serviceInterest === 'Others' && (
         <div className="mb-4">
-          <label htmlFor="serviceDetails" className="block text-gray-600 font-semibold">Service Details</label>
+          <label htmlFor="serviceDetails" className="block mb-2 text-gray-600 font-semibold">Service Details</label>
           <textarea
             {...register('serviceDetails', { required: 'Service Details are required' })}
             id="serviceDetails"
             placeholder="Enter Service Details"
-            className={`w-full px-4 py-2 border rounded-lg focus:outline-none ${
+            className={`w-full px-4 py-2 border  focus:outline-none ${
               errors.serviceDetails ? 'border-red-500' : 'focus:border-blue-400'
             }`}
           ></textarea>
@@ -136,12 +153,12 @@ const ContactForm = () => {
 
       
       <div className="mb-4">
-        <label htmlFor="Message" className="block text-gray-600 font-semibold">Message</label>
+        <label htmlFor="Message" className="block mb-2 text-gray-600 font-semibold">Message</label>
         <textarea
           {...register('Message', { required: 'Message is required' })}
           id="Message"
           placeholder="Enter a Message"
-          className={`w-full px-4 py-2 border rounded-lg focus:outline-none ${
+          className={`w-full px-4 py-2 border  focus:outline-none ${
             errors.Message ? 'border-red-500' : 'focus:border-blue-400'
           }`}
         ></textarea>
@@ -150,11 +167,29 @@ const ContactForm = () => {
         )}
       </div>
       
-      <div>
-        <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 focus:outline-none">Submit</button>
+      <div className='mb-10 text-center'>
+        <button type="submit" className="bg-[#225559] hover:bg-transparent border-2 border-transparent hover:border-[#225559] text-white hover:text-[#35868b] py-2 px-4 rounded-full duration-300">Request Appoinment</button>
       </div>
     </form>
+  </div>
+
+  {/*  Contact information */}
+  <div className="w-1/3 mx-auto px-4 mt-10 mr-7">
+    <div className="p-4 border rounded-lg bg-gray-100">
+      <h2 className="text-lg font-semibold mb-4">Contact Information</h2>
+      <p className="text-gray-600"><span className='font-bold'>Email:</span> Admin@legalco.com</p>
+      <p className="text-gray-600"><span className='font-bold'>Phone:</span> +88 01773239086</p>
+      <p className="text-gray-600"><span className='font-bold'>Address:</span> SEL Trident Tower, Suite # 907 (9th Floor), 57, Purana <br></br> Paltan, (VIP Road), Dhaka - 1000.</p>
+      
+      
+    </div>
+  </div>
+</div>
+    </div>
+    </div>
   );
 };
 
 export default ContactForm;
+
+
