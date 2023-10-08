b8f5dff323ec33da0e6478c25b284446de9f6bd4
jmjubaer <jmjubaer3927@gmail.com>
2023-10-08 17:46:21 +0600

    add

---
+++ src/app/api/services/[id]/route.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import services from '@/data/services.json'
+import { ObjectId } from "mongodb";
+import DbConnect from "@/Services/DbConnect";
+export const GET = async(request,{params}) => {
+    try{
+        const id = params?.id
+        console.log(id);
+        const db = await DbConnect();
+        const servicesCollection = db.collection('services');
+        const query = {_id: new ObjectId(id)}
+        const result = await servicesCollection.findOne(query);
+        return NextResponse.json(result);
+    } catch(e){
+        console.log(e.message);
+        NextResponse.json({ error: e.message });
+    }
+}
