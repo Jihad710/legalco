1241edcd09ef55fa373c25848e0ecc5dc560eeec
jmjubaer <jmjubaer3927@gmail.com>
2023-10-14 08:43:17 +0600

    add

---
+++ src/app/addlawyer/page.js
@@ -0,0 +1,18 @@
+import Container from '@/Common/Container';
+import React from 'react';
+
+const AddLawyerPage = () => {
+    return (
+        <div>
+            <div className="team_banner_bg_imgae h-[450px] -mt-24 text-white flex items-end justify-center">
+
+            </div>
+
+            <div className="">
+                
+            </div>
+        </div>
+    );
+};
+
+export default AddLawyerPage;
