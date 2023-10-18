bced20296906f56a145aff663f045f1335dff496
@u9KqBJdRPsTFXT <legalcoa668@gmail.com>
2023-10-19 00:37:42 +0600

    starting

--- src/Home/Services.js
+++ src/Home/Services.js
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 import Container from '@/Common/Container';
 import SectionTitle from '@/Common/SectionTitle';
 import { merriweather } from '@/Utils/Font';
@@ -7,32 +7,35 @@ import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 
 const ServicesPage = () => {
-    const [services,setServices] = useState([]);
-    useEffect(()=> {
-        (async()=>{
-            const res = await axios('/api/services')
-            setServices(res?.data);
-        })()
-    }, [])
-  
-    return (
-        <section className='py-20 bg_imgae'>
-            <Container>
-                <div className="text_shadow-white">
-                    <SectionTitle title={"Our Services"}></SectionTitle>
-                </div>
-                {/* */}
-                <div className="mt-10 grid grid-cols-3 gap-5">
-                    {
-                        services?.map((service,idx) => <div key={idx} className='border-2 bg-white shadow-lg p-5 flex items-center gap-5 rounded-2xl rounded-tr-none rounded-bl-none '>
-                            <Image width={50} height={50} src={service?.icon} alt='icon' className=''/>
-                            <h3 className={`${merriweather.className} font-medium text-lg`}>{service?.service}</h3>
-                        </div>)
-                    }
-                </div>
-            </Container>
-        </section>
-    );
+	const [services, setServices] = useState([]);
+	useEffect(() => {
+		(async () => {
+			const res = await axios('/api/services');
+			setServices(res?.data);
+		})();
+	}, []);
+
+	return (
+		<section className="py-20 bg_imgae">
+			<Container>
+				<div className="text_shadow-white">
+					<SectionTitle title={'Our Services'}></SectionTitle>
+				</div>
+				{/* */}
+				<div className="mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+					{services?.map((service, idx) => (
+						<div
+							key={idx}
+							className="border-2 bg-white shadow-lg p-5 flex items-center gap-5 rounded-2xl rounded-tr-none rounded-bl-none "
+						>
+							<Image width={50} height={50} src={service?.icon} alt="icon" className="" />
+							<h3 className={`${merriweather.className} font-medium text-lg`}>{service?.service}</h3>
+						</div>
+					))}
+				</div>
+			</Container>
+		</section>
+	);
 };
 
 export default ServicesPage;
--- src/Utils/Font.js
+++ src/Utils/Font.js
@@ -1,14 +1,13 @@
+import { Poppins, Bebas_Neue, Merriweather } from 'next/font/google';
 
-import { Poppins, Bebas_Neue, Merriweather } from 'next/font/google'
- 
 export const poppins = Poppins({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: ['400','500','600','700']
-})
- 
-export const merriweather =  Merriweather({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: ['300','400','700','900']
-})
+	subsets: ['latin'],
+	display: 'swap',
+	weight: ['400', '500', '600', '700']
+});
+
+export const merriweather = Merriweather({
+	subsets: ['latin'],
+	display: 'swap',
+	weight: ['300', '400', '700', '900']
+});
